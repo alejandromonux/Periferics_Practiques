de022f44e460db80a73ac74c0b53891be5659a1a
alejandromonux <alejandro.monux@students.salle.url.edu>
2022-04-09 16:49:55 +0200

    Fix: Input clock added

--- Practica_1/src/main.c
+++ Practica_1/src/main.c
@@ -143,13 +143,14 @@ void EXTI2_IRQHandler()
 }
 
 void INIT_IO_PRACTICA_1(){
+	  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 	  //Outputs
 	  GPIO_InitTypeDef GPIO_InitStructure;
 	  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6;
 	  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
 	  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
 	  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN ;
+	  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL; //FIXME: Si no va poner pullup
 	  GPIO_Init(GPIOD, &GPIO_InitStructure);
 	  //INPUTS
 	  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_2;
