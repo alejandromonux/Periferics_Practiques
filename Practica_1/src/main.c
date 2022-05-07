723205be319712d176cd19f52544e537b3779ae4
Noctis09 <52698486+Inane09@users.noreply.github.com>
2022-05-07 15:46:39 +0200

    Feat: USART Configuration

--- Practica_1/src/main.c
+++ Practica_1/src/main.c
@@ -302,6 +302,21 @@ void INIT_IO_PRACTICA_1(){
 	  NVIC_InitStruct2.NVIC_IRQChannelSubPriority = 0x05;
 	  NVIC_InitStruct2.NVIC_IRQChannelCmd = ENABLE;
 	  NVIC_Init(&NVIC_InitStruct2);
+
+}
+
+void INIT_USART(void){
+	USART_InitTypeDef USART_InitStructure;
+
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2,ENABLE);
+
+	USART_InitStructure.USART_BaudRate = 115200;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_Init(USART2, &USART_InitStructure);
 }
 
 int main(void)
