e81b001d4526dc415b728c01eca7b31722f5d372
alejandromonux <alejandro.monux@students.salle.url.edu>
2022-05-12 00:06:05 +0200

    Fix: USART1 is working

--- Practica_1/src/main.c
+++ Practica_1/src/main.c
@@ -312,18 +312,18 @@ void INIT_USART(void){
 	GPIO_InitTypeDef GPIO_InitStructure;
 
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2,ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
 
 
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-	GPIO_PinAFConfig(GPIOA,GPIO_PinSource2,GPIO_AF_USART2);
-	GPIO_PinAFConfig(GPIOA,GPIO_PinSource3,GPIO_AF_USART2);
+	GPIO_PinAFConfig(GPIOA,GPIO_PinSource9,GPIO_AF_USART1);
+	GPIO_PinAFConfig(GPIOA,GPIO_PinSource10,GPIO_AF_USART1);
 
 
 	USART_InitStructure.USART_BaudRate = /*128000*/ 9600;
@@ -332,8 +332,8 @@ void INIT_USART(void){
 	USART_InitStructure.USART_Parity = USART_Parity_No;
 	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
 	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-	USART_Init(USART2, &USART_InitStructure);
-	USART_Cmd(USART2, ENABLE);
+	USART_Init(USART1, &USART_InitStructure);
+	USART_Cmd(USART1, ENABLE);
 }
 
 int main(void)
@@ -364,10 +364,10 @@ int main(void)
 		  while(STM_EVAL_PBGetState(BUTTON_USER)==1){}
 	  }
 
-	  if(USART_GetFlagStatus(USART2,USART_IT_RXNE)==SET)
+	  if(USART_GetFlagStatus(USART1,USART_IT_RXNE)==SET)
 	{
-		uint16_t ucTemp = USART_ReceiveData( USART2 );
-		USART_SendData(USART2,ucTemp);
+		uint16_t ucTemp = USART_ReceiveData(USART1);
+		USART_SendData(USART1,ucTemp);
 		STM_EVAL_LEDToggle(LED4);
 	}
   }
