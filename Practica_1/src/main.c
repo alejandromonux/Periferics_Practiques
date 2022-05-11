1a2223298e41f00c216db43732dada96917863ce
alejandromonux <alejandro.monux@students.salle.url.edu>
2022-05-11 23:20:32 +0200

    Fix: USART change that's supposed to better it

--- Practica_1/src/main.c
+++ Practica_1/src/main.c
@@ -315,14 +315,15 @@ void INIT_USART(void){
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2,ENABLE);
 
 
-	GPIO_PinAFConfig(GPIOA,GPIO_PinSource2,GPIO_AF_USART2);
-	GPIO_PinAFConfig(GPIOA,GPIO_PinSource3,GPIO_AF_USART2);
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	GPIO_PinAFConfig(GPIOA,GPIO_PinSource2,GPIO_AF_USART2);
+	GPIO_PinAFConfig(GPIOA,GPIO_PinSource3,GPIO_AF_USART2);
 
 
 	USART_InitStructure.USART_BaudRate = /*128000*/ 9600;
