ca4765919cef3cb963cd7fa9a57c6b798190a717
alejandromonux <alejandro.monux@students.salle.url.edu>
2022-05-28 19:08:31 +0200

    Feat: Pasar del buffer a memoria principal

--- Practica_1/src/DMA_usart_config.c
+++ Practica_1/src/DMA_usart_config.c
@@ -12,7 +12,7 @@ void configUsart(int dataAmount){
 	DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t) &USART1->DR;
 	DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)&UsartIncomingThingies[0];
 	DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralToMemory;
-	DMA_InitStructure.DMA_BufferSize = 2;
+	DMA_InitStructure.DMA_BufferSize = 512;
 	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
 	DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
 	DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
@@ -43,13 +43,113 @@ void configUsart(int dataAmount){
 
 	/* CONFIGURACIÓN PARA EL TX (STREAM 7) */
 
-	/*Tornem a encendre la DMA*/
-
+	/*Configurem la memòria dinàmica*/
+	data = (Data *) malloc(sizeof(Data));
+	pendingData = 0;
+	totalSize = 1;
+	anteriorFinal=0;
+	anteriorBuffer=0;
 }
+
+
+//Es possible que arribin vàries mostres abans de que aquesta funció es faci per primer cop si arriben mostres però no
+//Plenen ningún dels dos buffers
+char isMostraPlena(){
+	//TODO: Potser podem guardar aquí en un array auxiliar la mostra i passarla al emplena i encua sense tant embolic
+	int bufferActual = anteriorBuffer;
+	uint8_t hemMiratInici=0;
+	uint8_t hemMiratNumDades=0;
+	uint8_t quantesDades=255; //FIXME:Mirar que no pueda ser 0 por l ode pillar valores anteriores al [6]
+	uint8_t numDades= 0;
+	uint8_t * buffer = (bufferActual==0)?UsartIncomingThingies:UsartIncomingThingies2;
+	int i = 0;
+	for(i =anteriorFinal; numDades!=quantesDades;i++){
+		//Mirem si no ens hem passat del tamany del buffer (igual al tamany de la cua
+		if(i>MAX_CUASIZE-1){
+			bufferActual = 1-bufferActual;
+			i=0;
+			buffer = (bufferActual==0)?UsartIncomingThingies:UsartIncomingThingies2;
+		}
+
+		//Mirem si tenim mostra sencera (
+		//Realment si canvia de buffer no passa res perque son adreçes contígues. PERÒ potser mirar de com fer per si no ho fossin perque si es del 2 al 1 cagada
+		//Mirem més tard que no hagi saltat el interrupt amb només el 0x40 guardat [linia 102]
+		if((buffer[i]==0x40)&&(buffer[i+1]==0x81) ){
+			if((!hemMiratInici)){
+				hemMiratInici=1;
+			}else if (numDades==quantesDades){
+				//MAI ENTRARÀ PER LA CONDICIÓ DEL FOR??
+				/*if(quantesDades!=255){
+					lastDataRead[numDades]='\0'; //AIXÍ SABEM FINS ON LLEGIR
+				}
+				return 1; //TRUE
+				*/
+			}else{
+				return 0; //FALSE
+			}
+		}
+
+
+		if(hemMiratNumDades<6){
+			//Apuntem quantes Mostres
+			quantesDades= buffer[i];
+			hemMiratNumDades++;
+		}else{
+			//Anem emplenant l'array
+			lastDataRead[numDades++]=buffer[i];
+		}
+	}
+	//Comprovem bé que no llegim res de brutícia per lo de canviar de buffer o que hagi saltat el interrupt amb només el 0x40 guardat
+	if (hemMiratIinici==0){
+		return 0;
+	}else if(quantesDades!=255){
+		lastDataRead[numDades]='\0'; //AIXÍ SABEM FINS ON LLEGIR
+	}
+	anteriorFinal=i;
+	return 1;
+}
+void emplenaIEncua(Data * array, unsigned int index){
+	for(int i = 0;MAX_DATASIZE;i++){
+		if(lastDataRead[i]!='\0'){
+			array[index]->data[i] = lastDataRead[i];
+			i++;
+		}else{
+			break;
+		}
+	}
+	cua[indexCua]=index;
+	indexCua = (indexCua+1)%MAX_CUASIZE;
+}
+Data desencua(){
+	posicio = (posicio+1)%MAX_CUASIZE;
+	return dataArray[cua[posicio++]];
+}
+
+
 
 void DMA2_Stream5_IRQHandler()
 {
 	STM_EVAL_LEDToggle(LED4);
+	/*MIRAR SI LA MUESTRA ESTÁ LLENA*/
+	if(isMostraPlena()){
+		if (pendingData == totalSize){
+			//Realloc
+			dataArray = (Data *) realloc(dataArray, sizeof(Data)*(++totalSize));
+			emplenaIencua(dataArray, totalSize-1);
+		}else{
+			for(unsigned int i = 0; i<totalSize;){
+				/*Mirar si nos da para los datos actuales o no*/
+				if(dataArray[i].used==1){
+					i++;
+				}else{
+					//Ficar dades i assignar posicio de la cua
+					emplenaIencua(dataArray,i);
+				}
+			}
+		}
+		pendingData++;
+	}
+
 	// Netejem la flag
 	DMA_ClearFlag(DMA2_Stream5, DMA_FLAG_TCIF5);
 	DMA_ClearITPendingBit(DMA2_Stream5, DMA_IT_TCIF5);
--- Practica_1/src/DMA_usart_config.h
+++ Practica_1/src/DMA_usart_config.h
@@ -4,9 +4,32 @@
 #include "stm32f4xx.h"
 #include "stm32f429i_discovery.h"
 
+
+
 #define DMA_REGISTERBOUNDARY_ADDRESS     ((uint32_t)0x40011000) /*IGUAL NO ESTÁ BIEN. SACADO DE	PAG 89 DEL DATASHEET*/
-__IO uint8_t UsartIncomingThingies[2]; //Cosicas
-__IO uint8_t UsartIncomingThingies2[2];
+__IO uint8_t UsartIncomingThingies[512]; //Cosicas
+__IO uint8_t UsartIncomingThingies2[512];
+unsigned int anteriorFinal;
+unsigned int anteriorBuffer;
+/*Arrays donde guardaremos la info*/
+#define MAX_DATASIZE 255
+typedef struct{
+	uint8_t used;
+	uint8_t datasize;
+	uint16_t data[MAX_DATASIZE]; //2 Bytes per mostra
+}Data;
+Data * dataArray;
+unsigned int totalSize;
+unsigned int pendingData;
+uint16_t lastDataRead[MAX_DATASIZE];
+
+/*pseudo-Cola buena para saber cuál printar ahora*/
+#define MAX_CUASIZE 512
+unsigned int posicio;
+unsigned char cua[MAX_CUASIZE]; //Num arbitrari
+unsigned int indexCua; //Pot ser que el index doni la volta i avanci la posició??
+
+
 void configUsart(int dataAmount);
 
 #endif
