c8ca032086e55aed1de54d0c64eaea982566cb7b
alejandromonux <alejandro.monux@students.salle.url.edu>
2022-05-30 16:51:39 +0200

    Feat: Functions implemented

--- Practica_1/src/DATA_processing.c
+++ Practica_1/src/DATA_processing.c
@@ -6,3 +6,39 @@
  */
 
 
+float getSampleFrequency(uint8_t data){
+	return (float) (data >> 1)/10;
+}
+
+float getAngle(uint16_t data){
+	return (float) (data >> 1)/64;
+}
+
+float getDiferenciaAngles(Data data){
+	float angle_dif = getAngle(data.angleInicial) - getAngle(data.angleFinal);
+	if (angle_dif < 0) angle_dif += 360;
+	return angle_dif;
+}
+
+uint16_t getChecksum(Data data){
+	uint16_t checksum = 0x8140;
+	checksum ^= data.angleFinal;
+	for (int i =0; i < 2*data.datasize; i+2){
+		checksum ^= (uint16_t) (data.data[i]<<8|data[i]);
+	}
+	checksum ^= (uint16_t) (data.datasize <<8 | data.datasize= 0x01);
+	checksum ^= data.angleInicial;
+	return checksum;
+}
+float getAngleMostra(uint16_t angleInicial, uint16_t numMostra, float incrementAngle){
+	return incrementAngle*numMostra +angleInicial;
+}
+
+Mesura getMesura(uint16_t data, uint16_t numMostra,float increment){
+	Mesura out;
+
+	out.angle = getAngleMostra(data.angleInicial,numMostra, increment);
+	out.distancia = (float) (data.data[numMostra+1]<<8)|data.data[numMostra];
+
+	return out;
+}
--- Practica_1/src/DATA_processing.h
+++ Practica_1/src/DATA_processing.h
@@ -9,11 +9,16 @@
 #define DATA_PROCESSING_H_
 #include <DMA_usart_config.h>
 
+typedef struct{
+	float angle;
+	float distancia;
+}Mesura;
+
 float getSampleFrequency(uint8_t data);
 float getAngle(uint16_t data);
 float getDiferenciaAngles(Data data);
-float getChecksum(Data data);
+uint16_t getChecksum(Data data);
 float getAngleMostra(uint16_t numMostra);
-float getMesura(uint16_t data, uint16_t numMostra);
+Mesura getMesura(uint16_t data, uint16_t numMostra, float angleIncrement);
 
 #endif /* DATA_PROCESSING_H_ */
